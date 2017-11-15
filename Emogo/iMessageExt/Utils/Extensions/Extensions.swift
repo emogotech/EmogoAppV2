d9e1b688518240f7a716f08c041a90ea7f7302e0
Vikas Goyal <vikas.goyal@northout.com>
2017-11-15 19:34:07 +0530

    added folder structure for iMessage.

---
+++ Emogo/iMessageExt/Utils/Extensions/Extensions.swift
@@ -0,0 +1,270 @@
+//
+//  Extensions.swift
+//  Emogo
+//
+//  Created by Vikas Goyal on 15/11/17.
+//  Copyright © 2017 Vikas Goyal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CRNotifications
+
+
+// MARK: - UIColor
+extension UIColor {
+    
+    convenience init (r : CGFloat , g : CGFloat , b : CGFloat ) {
+        self.init(red: r / 255 , green: g / 255 , blue: b / 255 , alpha: 1.0)
+    }
+    
+}
+
+// MARK: - UIView
+extension UIView {
+    
+    func addCorner (radius : CGFloat , borderWidth : CGFloat , color : UIColor ) {
+        
+        self.layer.cornerRadius = radius
+        self.layer.borderWidth = borderWidth
+        self.layer.borderColor = color.cgColor
+        self.layer.masksToBounds = true
+        
+    }
+    
+    var shadow: Bool {
+        get {
+            return layer.shadowOpacity > 0.0
+        }
+        set {
+            if newValue == true {
+                self.addShadow()
+            }
+        }
+    }
+    
+    var cornerRadius: CGFloat {
+        get {
+            return self.layer.cornerRadius
+        }
+        set {
+            self.layer.cornerRadius = newValue
+            // Don't touch the masksToBound property if a shadow is needed in addition to the cornerRadius
+            if shadow == false {
+                self.layer.masksToBounds = true
+            }
+        }
+        
+    }
+    
+    func addShadow(shadowColor: CGColor = UIColor.darkGray.cgColor,shadowOffset: CGSize = CGSize(width: 1.0, height: 2.0),shadowOpacity: Float = 0.6, shadowRadius: CGFloat = 3.0) {
+        layer.shadowColor = shadowColor
+        layer.shadowOffset = shadowOffset
+        layer.shadowOpacity = shadowOpacity
+        layer.shadowRadius = shadowRadius
+    }
+    
+    
+    func swipeToUp (height : CGFloat ) {
+        
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 8.0, initialSpringVelocity: 8.0, options: .curveEaseIn, animations: {
+            
+            self.frame.origin.y -= height
+            
+        }, completion: nil )
+        
+    }
+    
+    func swipeToDown (height : CGFloat ) {
+        
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 8.0, initialSpringVelocity: 8.0, options: .curveEaseIn, animations: {
+            
+            self.frame.origin.y += height
+            
+        }, completion: nil )
+        
+    }
+    
+    
+    func swipeToRight (distance : CGFloat ) {
+        
+        UIView.animate(withDuration: 0.3, delay: 0.0, usingSpringWithDamping: 5.0, initialSpringVelocity: 5.0, options: .curveEaseIn, animations: {
+            
+            self.frame.origin.x += distance
+            
+        }, completion: nil )
+        
+    }
+    
+    func swipeToLeft (distance : CGFloat ) {
+        
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseIn, animations: {
+            
+            self.frame.origin.x -= distance
+            
+        }, completion: nil )
+        
+    }
+    
+    
+    func makeClickEffect () {
+        
+        UIView.animate(withDuration: 0.2,
+                       animations: {
+                        self.transform = CGAffineTransform(scaleX: 0.95 , y: 0.95)
+        },
+                       completion: { _ in
+                        
+                        UIView.animate(withDuration: 0.2) {
+                            self.transform = CGAffineTransform.identity
+                        }
+                        
+        })
+        
+    }
+    
+    
+    func fadeIn(duration: TimeInterval ) {
+        
+        UIView.animate(withDuration: duration, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations:  {
+            
+            self.alpha = 1.0
+            
+        }, completion: nil)
+        
+    }
+    
+    func fadeOut(duration: TimeInterval ) {
+        
+        UIView.animate(withDuration: duration , delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations:  {
+            
+            self.alpha = 0.0
+            
+        }, completion: nil)
+        
+    }
+    
+}
+
+// MARK: - String
+extension String {
+    
+    func stringByAddingPercentEncodingForURLQueryParameter() -> String? {
+        let allowedCharacters = NSCharacterSet.urlQueryAllowed
+        return addingPercentEncoding(withAllowedCharacters: allowedCharacters)
+    }
+    
+    func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height:height )
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font: font], context: nil)
+        return boundingBox.width
+    }
+    func trim() -> String{
+        return self.trimmingCharacters(in: CharacterSet.whitespaces)
+    }
+    
+}
+
+// MARK: - String
+extension UIView {
+    
+}
+
+
+// MARK: - UIButton
+extension UIButton {
+    
+}
+
+// MARK: - UIButton
+
+extension UIViewController {
+    
+    func showAlert(strMessage:String){
+        let alert = UIAlertController(title: "Message", message: strMessage, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .cancel) { (actoin) in
+        }
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func showToast(type:String,strMSG:String) {
+        if type == "1" {
+            CRNotifications.showNotification(type: .success, title: "Message!", message: strMSG, dismissDelay: 3)
+        }else if type == "2" {
+            CRNotifications.showNotification(type: .error, title: "Alert!", message: strMSG, dismissDelay: 3)
+        }else {
+            CRNotifications.showNotification(type: .info, title: "Info!", message: strMSG, dismissDelay: 3)
+        }
+        
+    }
+}
+
+// MARK: - UINavigationController
+
+extension UINavigationController {
+    
+    /**
+     Pop current view controller to previous view controller.
+     
+     - parameter type:     transition animation type.
+     - parameter duration: transition animation duration.
+     */
+    func pop(transitionType type: String = kCATransitionFade, duration: CFTimeInterval = 0.3) {
+        self.addTransition(transitionType: type, duration: duration)
+        self.popViewController(animated: false)
+    }
+    
+    /**
+     Push a new view controller on the view controllers's stack.
+     - parameter vc:       view controller to push.
+     - parameter type:     transition animation type.
+     - parameter duration: transition animation duration.
+     */
+    func push(viewController vc: UIViewController, transitionType type: String = kCATransitionFade, duration: CFTimeInterval = 0.3) {
+        self.addTransition(transitionType: type, duration: duration)
+        self.pushViewController(vc, animated: false)
+    }
+    
+    func flipPush(viewController vc: UIViewController, transitionType type: String = "cube", duration: CFTimeInterval = 0.8) {
+        self.addFlipTransition(transitionType: type, duration: duration)
+        self.pushViewController(vc, animated: false)
+    }
+    
+    private func addTransition(transitionType type: String = kCATransitionFade, duration: CFTimeInterval = 0.3) {
+        let transition = CATransition()
+        transition.duration = duration
+        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        transition.type = type
+        self.view.layer.add(transition, forKey: nil)
+    }
+    
+    private func addFlipTransition(transitionType type: String = "cube", duration: CFTimeInterval = 0.8) {
+        let transition = CATransition()
+        transition.duration = duration
+        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        transition.type = type
+        transition.subtype = kCATransitionFromRight
+        self.view.layer.add(transition, forKey: nil)
+        
+    }
+}
+
+
+// MARK: UIKIT
+extension UITextField {
+    func shake() {
+        let animation : CABasicAnimation = CABasicAnimation(keyPath: "transform.translation.x")
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.repeatCount = 5
+        animation.duration = 0.1
+        animation.autoreverses = true
+        animation.byValue = -5
+        layer.add(animation, forKey: "shake")
+    }
+    
+    func placeholderColor(){
+        self.attributedPlaceholder = NSAttributedString(string: "placeholder text",
+                                                        attributes: [NSAttributedStringKey.foregroundColor: UIColor.white])
+    }
+}
