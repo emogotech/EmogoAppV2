157f5a2f49022c6d8a239139dc4b44e691bab777
Vikas Goyal <vikas.goyal@northout.com>
2017-11-15 17:15:28 +0530

    Added a User model and view.

---
+++ Emogo/Emogo/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Emogo
+//
+//  Created by Vikas Goyal on 15/11/17.
+//  Copyright © 2017 Vikas Goyal. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
