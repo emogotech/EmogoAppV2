606b4ae1f49a03bd300b598193792374b60c1b58
Sourabh <sourabh.gajbhiye@northout.com>
2017-12-11 12:07:39 +0530

    update for ui test

--- Emogo/EmogoUITests/EmogoCameraVCUITests.swift
+++ Emogo/EmogoUITests/EmogoCameraVCUITests.swift
@@ -25,6 +25,219 @@ class EmogoCameraVCUITests: XCTestCase {
         app         = nil
     }
     
+    func testToPerformHAppyCase(){
+        let btnSignIn = app.buttons["sign in btn"]
+        let txtPhone = app.textFields["Please enter phone number"]
+        let btnDone = app.buttons["done btn"]
+        
+        let btnCamera        = app.navigationBars.buttons["camera icon"]
+        let btnCaptureIcon   = app.buttons["capture icon"]
+
+        let btnShare = app.buttons["share button"]
+ 
+
+
+        
+        let prediatForHittable = NSPredicate(format: "isHittable == 1")
+//        expectation(for: prediatForHittable, evaluatedWith: btnSignIn, handler: nil)
+//        waitForExpectations(timeout: 10, handler: nil)
+//        btnSignIn.tap()
+//
+//        expectation(for: prediatForHittable, evaluatedWith: txtPhone, handler: nil)
+//        waitForExpectations(timeout: 10, handler: nil)
+//        txtPhone.clearAndEnterText(text: "9999999999")
+//        btnDone.tap()
+        
+//        sleep(2)
+        expectation(for: prediatForHittable, evaluatedWith: btnCamera, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
+        btnCamera.tap()
+        
+        
+        expectation(for: prediatForHittable, evaluatedWith: btnCaptureIcon, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
+        btnCaptureIcon.tap()
+
+        let collectionView = app.collectionViews.element.children(matching:.any)
+
+        sleep(1)
+        XCTAssertTrue( (collectionView.allElementsBoundByAccessibilityElement.count == 1 ), "Collection view not appeared!")
+
+        let picClicked  =   collectionView.allElementsBoundByAccessibilityElement.first
+
+        let btnGallery = app.buttons["add galery"]
+        btnGallery.tap()
+
+        let galleryCollectionViewCell = app.collectionViews.cells.element.children(matching: .any).element(boundBy: 0)
+
+        if galleryCollectionViewCell.exists {
+            galleryCollectionViewCell.forceTapElement()
+        }
+        let galleryNavBar = app.navigationBars["All Photos"]
+        let btnSelect1     = galleryNavBar.buttons["Select(1)"]
+
+        btnSelect1.tap()
+
+        sleep(2)
+        XCTAssertTrue( (collectionView.allElementsBoundByAccessibilityElement.count == 2 ), "Collection view does not have 2 images as expected!")
+
+        let picChoosed  =   collectionView.allElementsBoundByAccessibilityElement.first
+
+        XCTAssertFalse(( picClicked == picChoosed ), "Images are same that means updated image is not appearing in first position!")
+        
+        let btnBack         =   app.buttons["white back icon"]
+        expectation(for: prediatForHittable, evaluatedWith: btnBack, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
+        btnBack.tap()
+        
+        expectation(for: prediatForHittable, evaluatedWith: btnCamera, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
+        btnCamera.tap()
+        
+        expectation(for: prediatForHittable, evaluatedWith: btnCaptureIcon, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
+        btnCaptureIcon.tap()
+        
+        
+        sleep(1)
+        XCTAssertTrue( (collectionView.allElementsBoundByAccessibilityElement.count == 1 ), "Collection view not appeared!")
+        
+        let picClicked01  =   collectionView.allElementsBoundByAccessibilityElement.first
+        
+        btnGallery.tap()
+        
+        let galleryCollectionViewCell01 = app.collectionViews.cells.element.children(matching: .any).element(boundBy: 0)
+        
+        if galleryCollectionViewCell01.exists {
+            galleryCollectionViewCell01.forceTapElement()
+        }
+        
+        btnSelect1.tap()
+        
+        XCTAssertTrue( (collectionView.allElementsBoundByAccessibilityElement.count == 2 ), "Collection view does not have 2 images as expected!")
+        
+        let picChoosed01  =   collectionView.allElementsBoundByAccessibilityElement.first
+        
+        XCTAssertFalse(( picClicked01 == picChoosed01 ), "Images are same that means updated image is not appearing in first position!")
+        
+        expectation(for: prediatForHittable, evaluatedWith: btnShare, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
+        btnShare.tap()
+
+        
+        let collectionViewForEdit = app.collectionViews.element.children(matching:.any)
+
+        let seeconPicForEdit = collectionViewForEdit.allElementsBoundByAccessibilityElement.last
+        
+        seeconPicForEdit?.forceTapElement()
+        
+        sleep(2)
+        
+        let titleYourImageTextField = app.textFields["Title your Image"]
+        expectation(for: prediatForHittable, evaluatedWith: titleYourImageTextField, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
+        
+        titleYourImageTextField.forceTapElement()
+        titleYourImageTextField.clearAndEnterText(text: "Sourabh")
+        titleYourImageTextField.typeText("\n")
+        
+        let descriptionTextTextField = app.textFields["Description text"]
+        descriptionTextTextField.clearAndEnterText(text: "Sourabh Desc")
+        app.typeText("\n")
+        
+        let doneButton = app.buttons["  Done"]
+        
+        doneButton.tap()
+        
+        let editIconButton = app.buttons["edit icon"]
+        expectation(for: prediatForHittable, evaluatedWith: editIconButton, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
+        editIconButton.tap()
+        
+        
+        
+        let pencilButton = app.buttons["pencil icon"]
+        expectation(for: prediatForHittable, evaluatedWith: pencilButton, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
+        pencilButton.tap()
+        
+        sleep(1)
+        
+        
+        let cooridnate1 = app.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 0)).withOffset(CGVector(dx: 20 , dy: 100))
+        let cooridnate2 = app.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 0)).withOffset(CGVector(dx: 40 , dy: 300))
+        let cooridnate3 = app.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 0)).withOffset(CGVector(dx: 70 , dy: 200))
+        let cooridnate4 = app.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 0)).withOffset(CGVector(dx: 250 , dy: 500))
+        let cooridnate5 = app.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 0)).withOffset(CGVector(dx: 199 , dy: 450))
+        let cooridnate6 = app.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 0)).withOffset(CGVector(dx: 150 , dy: 100))
+
+        
+        
+        cooridnate1.tap()
+        cooridnate2.tap()
+        cooridnate3.tap()
+        cooridnate4.tap()
+        cooridnate5.tap()
+        cooridnate6.tap()
+        
+        
+        cooridnate1.press(forDuration: TimeInterval.init(exactly: 0)!, thenDragTo: cooridnate2)
+        cooridnate2.press(forDuration: TimeInterval.init(exactly: 0)!, thenDragTo: cooridnate3)
+        cooridnate3.press(forDuration: TimeInterval.init(exactly: 0)!, thenDragTo: cooridnate4)
+        cooridnate4.press(forDuration: TimeInterval.init(exactly: 0)!, thenDragTo: cooridnate5)
+        cooridnate5.press(forDuration: TimeInterval.init(exactly: 0)!, thenDragTo: cooridnate6)
+
+        
+        sleep(1)
+        let colorBucketIconUnactiveButton = app.buttons["color bucket icon unactive"]
+        colorBucketIconUnactiveButton.tap()
+        
+        
+        let collectionViewColors = app.collectionViews.element.children(matching:.any)
+        let count = collectionViewColors.count
+        print(count)
+        
+        
+        for var i in 0..<count{
+            if i == 0 {
+                
+            }else if i == 1 {
+                
+            }else if i == 2 {
+                
+            }else if i == 3 {
+                
+            }else if i == 4 {
+                
+            }else if i == 1 {
+                
+            }
+        }
+        for cell in  collectionViewColors.allElementsBoundByAccessibilityElement {
+            cell.tap()
+            
+            sleep(1)
+            cooridnate1.press(forDuration: TimeInterval.init(exactly: 0)!, thenDragTo: cooridnate2)
+            cooridnate2.press(forDuration: TimeInterval.init(exactly: 0)!, thenDragTo: cooridnate3)
+            cooridnate3.press(forDuration: TimeInterval.init(exactly: 0)!, thenDragTo: cooridnate4)
+            cooridnate4.press(forDuration: TimeInterval.init(exactly: 0)!, thenDragTo: cooridnate5)
+            cooridnate5.press(forDuration: TimeInterval.init(exactly: 0)!, thenDragTo: cooridnate6)
+
+        }
+        
+        
+        sleep(1)
+        
+        let btnEditingDone = app.buttons["editing done icon"]
+        btnEditingDone.tap()
+        let btnDownload = app.buttons["download icon"]
+        btnDownload.tap()
+        
+        
+        sleep(10)
+        
+    }
+    
     func testOpenCamera(){
         let btnCamera        = app.navigationBars.buttons["camera icon"]
         let btnCaptureIcon   = app.buttons["capture icon"]
@@ -110,10 +323,10 @@ class EmogoCameraVCUITests: XCTestCase {
         let btnGallery = app.buttons["add galery"]
         btnGallery.tap()
         
-        let gg = app.collectionViews.cells.element.children(matching: .any).element(boundBy: 0)
+        let galleryCollectionViewCell = app.collectionViews.cells.element.children(matching: .any).element(boundBy: 0)
         
-        if gg.exists {
-            gg.forceTapElement()
+        if galleryCollectionViewCell.exists {
+            galleryCollectionViewCell.forceTapElement()
         }
         let galleryNavBar = app.navigationBars["All Photos"]
         let btnSelect1     = galleryNavBar.buttons["Select(1)"]
@@ -146,10 +359,10 @@ class EmogoCameraVCUITests: XCTestCase {
         let btnGallery = app.buttons["add galery"]
         btnGallery.tap()
         
-        let gg = app.collectionViews.cells.element.children(matching: .any).element(boundBy: 0)
+        let galleryCollectionViewCell = app.collectionViews.cells.element.children(matching: .any).element(boundBy: 0)
         
-        if gg.exists {
-            gg.forceTapElement()
+        if galleryCollectionViewCell.exists {
+            galleryCollectionViewCell.forceTapElement()
         }
         let galleryNavBar = app.navigationBars["All Photos"]
         let btnSelect1     = galleryNavBar.buttons["Select(1)"]
@@ -257,17 +470,12 @@ class EmogoCameraVCUITests: XCTestCase {
         let predicate   =  NSPredicate(format: "exists == 1")
         
         expectation(for: predicate, evaluatedWith: btnBackEditImage, handler: nil)
-        waitForExpectations(timeout: 10, handler: nil)
+                waitForExpectations(timeout: 10, handler: nil)
         btnBackEditImage.tap()
         
-        XCTAssertTrue(btnShare.exists, "btnShare is not present not in Camer view")
+        XCTAssertTrue(btnShare.exists, "btnShare is not present not in Camera view")
         
         sleep(2)
-        
-    }
-    
-    func testBackkk(){
-        XCUIApplication().buttons["back circle icon"].tap()
         
     }
     
@@ -317,7 +525,7 @@ class EmogoCameraVCUITests: XCTestCase {
 
     }
     
-    func testCamera_Capture_Image_And_Edit_With_A__Color_Line_And_SaveToPhotoLibrary(){
+    func testCamera_Capture_Image_And_Edit_With_A_Color_Line_And_SaveToPhotoLibrary(){
         
         sleep(2)
         app.navigationBars.buttons["camera icon"].tap()
@@ -470,17 +678,29 @@ class EmogoCameraVCUITests: XCTestCase {
         doneButton.tap()
     }
     
-    func testCamera_Capture_Live_Image_And_Save(){
+    func testCamera_Capture_Live_Image_And_Save_And_Check_Description_And_Title(){
         
-        sleep(2)
-        app.navigationBars.buttons["camera icon"].tap()
-        app.buttons["capture icon"].tap()
-        app.buttons["share button"].tap()
+        let btnCamera        = app.navigationBars.buttons["camera icon"]
+        let btnCaptureIcon   = app.buttons["capture icon"]
+        let btnShare         = app.buttons["share button"]
+        let btnBack          = app.buttons["back circle icon"]
+        
+        let prediate = NSPredicate(format: "isHittable == 1")
+        expectation(for: prediate, evaluatedWith: btnCamera, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
+        btnCamera.tap()
+        
+        sleep(1)
+        btnCaptureIcon.tap()
+        
+        expectation(for: prediate, evaluatedWith: btnShare, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
+        btnShare.tap()
+
         
         let titleYourImageTextField = app.textFields["Title your Image"]
         titleYourImageTextField.clearAndEnterText(text: "Sourabh")
         titleYourImageTextField.typeText("\n")
-        
         
         let descriptionTextTextField = app.textFields["Description text"]
         descriptionTextTextField.clearAndEnterText(text: "Sourabh Desc")
@@ -488,19 +708,121 @@ class EmogoCameraVCUITests: XCTestCase {
         
         let doneButton = app.buttons["  Done"]
         doneButton.tap()
+        
+        expectation(for: prediate, evaluatedWith: btnBack, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
+
+        
+        btnBack.tap()
+        
+        sleep(1)
+        
+        btnShare.tap()
+        
+        XCTAssertEqual(titleYourImageTextField.value as! String, "Sourabh", "Title is not same as entered.")
+        XCTAssertEqual(descriptionTextTextField.value as! String, "Sourabh Desc", "description is not same as entered.")
+        
+    }
+    
+    func testCameraWithTwoImagesWithTheirDistincness(){
+        
+        let btnCamera        = app.navigationBars.buttons["camera icon"]
+        let btnCaptureIcon   = app.buttons["capture icon"]
+        let btnShare         = app.buttons["share button"]
+        let btnBack          = app.buttons["back circle icon"]
+        
+        let prediate = NSPredicate(format: "isHittable == 1")
+        expectation(for: prediate, evaluatedWith: btnCamera, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
+        btnCamera.tap()
+        
+        sleep(1)
+        btnCaptureIcon.tap()
+        
+        expectation(for: prediate, evaluatedWith: btnShare, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
+        btnShare.tap()
+        
+        
+        let titleYourImageTextField = app.textFields["Title your Image"]
+        titleYourImageTextField.clearAndEnterText(text: "Sourabh")
+        titleYourImageTextField.typeText("\n")
+        
+        let descriptionTextTextField = app.textFields["Description text"]
+        descriptionTextTextField.clearAndEnterText(text: "Sourabh Desc")
+        app.typeText("\n")
+        
+        let doneButton = app.buttons["  Done"]
         doneButton.tap()
+        
+        expectation(for: prediate, evaluatedWith: btnBack, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
+        
+        
+        btnBack.tap()
+        
+        expectation(for: prediate, evaluatedWith: btnCaptureIcon, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
+        btnCaptureIcon.tap()
+        
+        expectation(for: prediate, evaluatedWith: btnShare, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
+        btnShare.tap()
+        
+        
+        let collectionView = app.collectionViews.element.children(matching:.any)
+        print(collectionView.allElementsBoundByAccessibilityElement)
+        XCTAssertEqual(collectionView.allElementsBoundByAccessibilityElement.count, 2, "Should be 2 images in collection view")
+
+        
+        let firstPic    =   collectionView.allElementsBoundByAccessibilityElement.first
+        firstPic?.forceTapElement()
+        
+        XCTAssertEqual(titleYourImageTextField.value as! String, "Title your Image" , "Title Should be not appear for Recently added image.")
+        XCTAssertEqual(descriptionTextTextField.value as! String, "Description text" , "Description Should be not appear for Recently added image.")
+        
+        let secondPic = collectionView.allElementsBoundByAccessibilityElement.last
+        
+        secondPic?.forceTapElement()
+        
+        XCTAssertEqual(titleYourImageTextField.value as! String, "Sourabh", "Title is not same as entered.")
+        XCTAssertEqual(descriptionTextTextField.value as! String, "Sourabh Desc", "description is not same as entered.")
+        
+        sleep(3)
+        
+    }
+    
+    func testForEmoji(){
+        
+        app.buttons["edit icon"].tap()
+        
+        let emojiIconButton = app.buttons["emoji icon"]
+        emojiIconButton.tap()
+        
+        let collectionView = app.scrollViews.children(matching: .collectionView).element
+
+        app?/*@START_MENU_TOKEN@*/.collectionViews.cells.staticTexts["😆"]/*[[".scrollViews.collectionViews",".cells.staticTexts[\"😆\"]",".staticTexts[\"😆\"]",".collectionViews"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,1]]@END_MENU_TOKEN@*/.swipeRight()
+        collectionView.swipeLeft()
+        app?/*@START_MENU_TOKEN@*/.collectionViews.cells.staticTexts["😃"]/*[[".scrollViews.collectionViews",".cells.staticTexts[\"😃\"]",".staticTexts[\"😃\"]",".collectionViews"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,1]]@END_MENU_TOKEN@*/.tap()
+        XCUIDevice.shared.orientation = .faceUp
         
     }
     
     func testCamera_Capture_Live_Image_And_Save_It_With_Text(){
         
+        let btnShare = app.buttons["share button"]
+        let editIconButton = app.buttons["edit icon"]
+
         sleep(2)
         app.navigationBars.buttons["camera icon"].tap()
         app.buttons["capture icon"].tap()
 
-        app.buttons["share button"].tap()
 
-        let editIconButton = app.buttons["edit icon"]
+        let predicateForShareButton = NSPredicate(format: "isHittable == 1")
+        expectation(for: predicateForShareButton, evaluatedWith: btnShare, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
+        
+        btnShare.tap()
 
         let predicate   =  NSPredicate(format: "exists == 1")
 
