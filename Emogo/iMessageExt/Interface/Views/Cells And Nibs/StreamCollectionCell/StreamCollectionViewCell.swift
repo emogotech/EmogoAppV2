05dde70b532089581f14b0441c5aba96cef8e671
Sushobhit <sushobhit.gupta@northout.com>
2017-12-11 18:49:20 +0530

    add view stream service with update UI.

--- Emogo/iMessageExt/Interface/Views/Cells And Nibs/StreamCollectionCell/StreamCollectionViewCell.swift
+++ Emogo/iMessageExt/Interface/Views/Cells And Nibs/StreamCollectionCell/StreamCollectionViewCell.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class StreamCollectionViewCell: UICollectionViewCell {
+  
     @IBOutlet weak var viewAddContent : UIView!
-    @IBOutlet weak var lblFoodName : UILabel!
-    @IBOutlet weak var imgFood : UIImageView!
+    @IBOutlet weak var imgCover: UIImageView!
+    @IBOutlet weak var lblName : UILabel!
+    
+    //@IBOutlet weak var btnPlay: UIButton!
+//    @IBOutlet weak var imgFood : UIImageView!
   
     override func awakeFromNib() {
         self.layer.cornerRadius = 5.0
         self.clipsToBounds = true
+    }
+    
+    func prepareLayout(content:ContentDAO){
+        if content.isAdd == true {
+            viewAddContent.isHidden = false
+        }else {
+            viewAddContent.isHidden  = true
+            self.lblName.text = content.name.trim().capitalized
+//            self.viewAddContent.layer.contents = UIImage(named: "gradient")?.cgImage
+            if content.type == "Picture" {
+              //  self.btnPlay.isHidden = true
+                self.imgCover.setImageWithURL(strImage: content.coverImage, placeholder: "stream-card-placeholder")
+            }else {
+             //   self.btnPlay.isHidden = false
+            }
+        }
+        
     }
 }
