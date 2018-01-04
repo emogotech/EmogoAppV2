9760e05c78ac153d992c078e43cabc03e3ee0614
Vikas Goyal <vikas.goyal@northout.com>
2018-01-04 17:15:20 +0530

    Add drafted for DSYM

--- Emogo/Emogo/AppDelegate.swift
+++ Emogo/Emogo/AppDelegate.swift
@@ -19,7 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        // Crashlytics
+        Fabric.with([Crashlytics.self])
+
         self.initializeApplication()
         return true
     }
@@ -119,8 +122,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             self.openLandingScreen()
         }
         self.keyboardToolBar(disable:false)
-        // Crashlytics
-        Fabric.with([Crashlytics.self])
     }
     
    fileprivate func openLandingScreen(){
