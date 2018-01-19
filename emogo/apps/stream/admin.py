48669e825d3b3352abc0bb995caddc965613b6a2
Ranjeet <ranjeet.bijore@northout.com>
2018-01-19 16:40:48 +0530

    Worked on Emogo Admin panel.

--- emogo/apps/stream/admin.py
+++ emogo/apps/stream/admin.py
@@ -2,5 +2,22 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from emogo.apps.stream.models import Stream, Content
 
+
+class StreamAdmin(admin.ModelAdmin):
+    model = Stream
+    search_fields = ('name',)
+    list_display = ['name', 'type',  'featured', 'emogo', 'created_by']
+
+
+class ContentAdmin(admin.ModelAdmin):
+    model = Content
+    list_display = ['get_name', 'type','url' ,'created_by']
+
+    def get_name(self, obj):
+        return obj.name
+
+admin.site.register(Stream, StreamAdmin)
+admin.site.register(Content, ContentAdmin)
 # Register your models here.
