262b2534dcc6f6f1771bdee7ab19023c1beeff05
Ranjeet <ranjeet.bijore@northout.com>
2018-02-09 12:51:38 +0530

    Add stream and content owner name in Admin panel.

--- emogo/apps/stream/admin.py
+++ emogo/apps/stream/admin.py
@@ -8,15 +8,23 @@ from emogo.apps.stream.models import Stream, Content
 class StreamAdmin(admin.ModelAdmin):
     model = Stream
     search_fields = ('name',)
-    list_display = ['name', 'type',  'featured', 'emogo', 'created_by']
+    list_display = ['name', 'type', 'featured', 'emogo', 'owner']
+
+    def owner(self, obj):
+        return obj.created_by.user_data.full_name
 
 
 class ContentAdmin(admin.ModelAdmin):
     model = Content
-    list_display = ['get_name', 'type','url' ,'created_by']
 
-    def get_name(self, obj):
+    list_display = ('name', 'type', 'url', 'owner',)
+
+    def name(self, obj):
         return obj.name
+
+    def owner(self, obj):
+        return obj.created_by.user_data.full_name
+
 
 admin.site.register(Stream, StreamAdmin)
 admin.site.register(Content, ContentAdmin)
