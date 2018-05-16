6f4d2772126c248e04433496586f3cbb8ca3e97a
Ranjeet <ranjeet.bijore@northout.com>
2018-05-16 15:10:32 +0530

    changes in custom middelware.

--- emogo/lib/common_middleware/stream_view_middleware.py
+++ emogo/lib/common_middleware/stream_view_middleware.py
@@ -35,20 +35,20 @@ class UpdateStreamViewCount(MiddlewareMixin):
                     stream_id = response.data.get('data').get('id')
                     suvs = StreamUserViewStatus.objects.create(user=request.user, stream_id=stream_id)
                     suvs.save()
-                    print('Counter Done')
-        if response.status_code == 200:
-            total_time = 0
-            for query in connection.queries:
-                print(query)
-                query_time = query.get('time')
-                if query_time is None:
-                    # django-debug-toolbar monkeypatches the connection
-                    # cursor wrapper and adds extra information in each
-                    # item in connection.queries. The query time is stored
-                    # under the key "duration" rather than "time" and is
-                    # in milliseconds, not seconds.
-                    query_time = query.get('duration', 0) / 1000
-                total_time += float(query_time)
-
-            print('%s queries run, total %s seconds' % (len(connection.queries), total_time))
+                    # print('Counter Done')
+        # if response.status_code == 200:
+        #     total_time = 0
+        #     for query in connection.queries:
+        #         print(query)
+        #         query_time = query.get('time')
+        #         if query_time is None:
+        #             # django-debug-toolbar monkeypatches the connection
+        #             # cursor wrapper and adds extra information in each
+        #             # item in connection.queries. The query time is stored
+        #             # under the key "duration" rather than "time" and is
+        #             # in milliseconds, not seconds.
+        #             query_time = query.get('duration', 0) / 1000
+        #         total_time += float(query_time)
+        #
+        #     print('%s queries run, total %s seconds' % (len(connection.queries), total_time))
         return response
