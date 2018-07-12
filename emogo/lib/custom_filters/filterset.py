ff3d6472d8d25690e86c0a553b15d1ed495875bd
Ranjeet <ranjeet.bijore@northout.com>
2018-07-12 14:59:11 +0530

    Fixed issues streams in profile section

--- emogo/lib/custom_filters/filterset.py
+++ emogo/lib/custom_filters/filterset.py
@@ -183,7 +183,7 @@ class UserStreamFilter(django_filters.FilterSet):
         stream_ids = Collaborator.actives.filter(stream__status='Active', created_by_id = user.id).values_list('stream', flat=True)
 
         # 2. Fetch and return stream Queryset objects without collaborators.
-        return  qs.exclude(id__in=stream_ids).filter(created_by__user_data__id= user.id, type='Public').order_by('-upd')
+        return  qs.exclude(id__in=stream_ids).filter(created_by_id= user.id, type='Public').order_by('-upd')
 
     def filter_collab_stream(self, qs, name, value):
         user = get_object_or_404(User, user_data__id=value)
