3653edc91d79c2b76a792650c1729813cf8b33f5
Swarnim <swarnim.shrotriya@northout.com>
2018-10-04 14:50:48 +0530

    Fixed stream content issues

--- emogo/apps/users/views.py
+++ emogo/apps/users/views.py
@@ -517,7 +517,7 @@ class UserCollaborators(ListAPIView):
         queryset = self.queryset.filter(id__in=Collaborator.actives.filter(Q(phone_number= self.request.user.username )| Q(created_by_id=self.request.user.id) ).values_list('stream_id', flat=True)).select_related('created_by__user_data__user').prefetch_related(
         Prefetch(
             "stream_contents",
-            queryset=StreamContent.objects.all().select_related('content').order_by('order').prefetch_related(
+            queryset=StreamContent.objects.all().select_related('content').order_by('order' , '-attached_date').prefetch_related(
                 Prefetch(
                     "content__content_like_dislike_status",
                     queryset=LikeDislikeContent.objects.filter(status=1),
