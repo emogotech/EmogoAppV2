b03a2f5682075d7374ce1302062b3ec775293d1f
Ranjeet <ranjeet.bijore@northout.com>
2018-11-15 15:14:15 +0530

    Add collaborators field in view stream

--- emogo/apps/stream/views.py
+++ emogo/apps/stream/views.py
@@ -156,10 +156,14 @@ class StreamAPI(CreateAPIView, UpdateAPIView, ListAPIView, DestroyAPIView, Retri
         :param kwargs: request param as dict
         :return: Update stream instance
         """
+        import pdb;
+        pdb.set_trace()
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
+        # import pdb;
+        # pdb.set_trace()
         self.perform_update(serializer)
         instance = self.get_object()
         self.serializer_class = ViewStreamSerializer
@@ -167,8 +171,6 @@ class StreamAPI(CreateAPIView, UpdateAPIView, ListAPIView, DestroyAPIView, Retri
         if version == 'v3':
             fields.remove('collaborators')
         serializer = self.get_serializer(instance, context=self.request, fields=fields)
-        if version == 'v3':
-            fields.remove('collaborators')
         if getattr(instance, '_prefetched_objects_cache', None):
             # If 'prefetch_related' has been applied to a queryset, we need to
             # forcibly invalidate the prefetch cache on the instance.
