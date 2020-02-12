fb0ff2cccef9c982281a939711aae020098c74d6
Ranjeet <ranjeet.bijore@northout.com>
2020-02-12 16:09:00 +0530

    Update cron file.

--- emogo/apps/collaborator/management/commands/update_collaborator.py
+++ emogo/apps/collaborator/management/commands/update_collaborator.py
@@ -2,18 +2,25 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from emogo.apps.collaborator.models import Collaborator
 import csv
+from django.db.models.functions import Length
+from django.db.models import Q
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
+
 
 class Command(BaseCommand):
     help = 'Update user entry for collaborator.'
 
     def handle(self, *args, **options):
         # Get all Collaborators data whose phone number length is greater than 10
-        collaborators = Collaborator.objects.filter(phone_number__gte=10)
+        collaborators = Collaborator.objects.annotate(phone_length=Length('phone_number')).filter(phone_length__gte=10)
         for collaborator in collaborators:
             try:
                 # Get user whose username is same as collaborators number
-                user = User.objects.get(username__endswith=collaborator.phone_number[-10:], username__gte=10)
+                user = User.objects.get(username__endswith=collaborator.phone_number[-10:])
                 collaborator.user = user
                 collaborator.save()
-            except:
+            except MultipleObjectsReturned:
                 pass
+            except ObjectDoesNotExist:
+                # user = User.objects.get(username__startswith=collaborator.phone_number[-10:])
+                print(collaborator.phone_number, 'Not found')
