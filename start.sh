79da9ec66851efdf05122ba5383cab8b7c58fcf6
Ranjeet <ranjeet.bijore@northout.com>
2020-03-24 19:16:08 +0530

    Worked on Stage Docker setup

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Start the run once job.
+echo "Docker container has been started"
+
+service rabbitmq-server start
+
+python3.6 manage.py collectstatic --settings=emogo.stage_settings --noinput
+#celery -A sporttechie worker -l info -B > /dev/null 2>&1 &
+python3.6 manage.py runserver 0.0.0.0:80 --settings=emogo.stage_settings
+
+exec "$@"
