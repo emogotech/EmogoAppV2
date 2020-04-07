87514ca06e722fcd9e1e7e7b6a56ee8cae44e20f
ranjeet-northout <ranjeet.bijore@northout.com>
2020-04-07 15:14:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # Start the run once job.
 echo "Docker container has been started"
 
-python3.6 manage.py collectstatic --settings=emogo.stage_settings
+python3.6 manage.py collectstatic --noinput --settings=emogo.stage_settings
 #celery -A sporttechie worker -l info -B > /dev/null 2>&1 &
 python3.6 manage.py runserver 0.0.0.0:80 --settings=emogo.stage_settings
 
