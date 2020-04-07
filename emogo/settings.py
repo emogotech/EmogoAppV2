a42e131983108be536cc5ee78b1353aa2e53b3df
Ranjeet <ranjeet.bijore@northout.com>
2020-04-07 10:43:28 +0530

    worked on setting file

--- emogo/settings.py
+++ emogo/settings.py
@@ -269,29 +269,29 @@ LOGGING = {
     },
 }
 
-DATABASES = {
-
-    "default": {
-        # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        # DB name or path to database file if using sqlite3.
-        "NAME": "emogo_v3",
-        # Not used with sqlite3.
-        "USER": "plannah_user",
-        # Not used with sqlite3.
-        "PASSWORD": "northout@123",
-        # Set to empty string for localhost. Not used with sqlite3.
-        "HOST": "127.0.0.1",
-        # Set to empty string for default. Not used with sqlite3.
-        "PORT": "5432",
-    },
-}
+# DATABASES = {
+#
+#     "default": {
+#         # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         # DB name or path to database file if using sqlite3.
+#         "NAME": "emogo_v3",
+#         # Not used with sqlite3.
+#         "USER": "plannah_user",
+#         # Not used with sqlite3.
+#         "PASSWORD": "northout@123",
+#         # Set to empty string for localhost. Not used with sqlite3.
+#         "HOST": "127.0.0.1",
+#         # Set to empty string for default. Not used with sqlite3.
+#         "PORT": "5432",
+#     },
+# }
 
 # Get Local Settings
 
-# try:
-#     from local_settings import *
-#     #Get PEM file url for notification
-#     NOTIFICATION_PEM_ROOT = os.path.join(BASE_DIR,PEM_FILE)
-# except ImportError:
-#     pass
+try:
+    from local_settings import *
+    #Get PEM file url for notification
+    # NOTIFICATION_PEM_ROOT = os.path.join(BASE_DIR,PEM_FILE)
+except ImportError:
+    pass
