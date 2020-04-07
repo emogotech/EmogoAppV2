4aad71e626f154657ee9382986c548ed0cedf79a
ranjeet-northout <ranjeet.bijore@northout.com>
2020-04-07 14:52:21 +0530

    Update settings.py

--- emogo/settings.py
+++ emogo/settings.py
@@ -290,7 +290,7 @@ LOGGING = {
 # Get Local Settings
 
 try:
-    from local_settings import *
+    from .local_settings import *
     #Get PEM file url for notification
     # NOTIFICATION_PEM_ROOT = os.path.join(BASE_DIR,PEM_FILE)
 except ImportError:
