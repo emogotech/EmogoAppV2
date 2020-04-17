dd13f08fecc9505209f1d4649831a45a5cceccdd
ranjeet-northout <ranjeet.bijore@northout.com>
2020-04-17 18:19:12 +0530

    Update stage_settings.py

--- emogo/stage_settings.py
+++ emogo/stage_settings.py
@@ -294,14 +294,21 @@ LOGGING = {
 # }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'emogodev',
-        'USER': 'northout',
-        'PASSWORD': 'PXkhMZm74MHwXp7M',
-        'HOST': 'emogo-devdb.czvdhrcaf6yc.us-east-1.rds.amazonaws.com',
-        'PORT': '5432',
-    }
+
+    "default": {
+        # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        # DB name or path to database file if using sqlite3.
+        "NAME": "stagedb",
+        # Not used with sqlite3.
+        "USER": "stagedba",
+        # Not used with sqlite3.
+        "PASSWORD": "2MSKRV38L5VV99QH",
+        # Set to empty string for localhost. Not used with sqlite3.
+        "HOST": "stagedba.cpoczs3mm579.us-east-1.rds.amazonaws.com",
+        # Set to empty string for default. Not used with sqlite3.
+        "PORT": "5432",
+    },
 }
 
 # Get Local Settings
