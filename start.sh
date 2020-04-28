2241acf35049b48bd2de89626b85382850754d84
Ranjeet <ranjeet.bijore@northout.com>
2020-04-28 19:30:56 +0530

    Update qa setting file

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 # Start the run once job.
 echo "Docker container has been started"
 
-#python3.6 manage.py collectstatic --yes --settings=emogo.stage_settings
-#python3.6 manage.py collectstatic --settings=emogo.stage_settings -y
+#python3.6 manage.py collectstatic --yes --settings=emogo.qa_settings
+#python3.6 manage.py collectstatic --settings=emogo.qa_settings -y
 
 #celery -A sporttechie worker -l info -B > /dev/null 2>&1 &
-python3.6 manage.py runserver 0.0.0.0:80 --settings=emogo.stage_settings
+python3.6 manage.py runserver 0.0.0.0:80 --settings=emogo.qa_settings
 
 exec "$@"
