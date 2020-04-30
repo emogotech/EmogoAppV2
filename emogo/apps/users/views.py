fa768fec2a90bdb50b98e90905b95bffcf50d1e0
Ranjeet <ranjeet.bijore@northout.com>
2020-04-30 13:10:25 +0530

    Enable logs

--- emogo/apps/users/views.py
+++ emogo/apps/users/views.py
@@ -1103,7 +1103,7 @@ class GetTopStreamAPIV3(ListAPIView):
 
         import logging
         logger_name = logging.getLogger('email_log')
-        logger_name.info(self.request)
+        logger_name.info('Request url: {0}'.format(self.request.META['wsgi.url_scheme']))
         qs = Stream.actives.all().annotate(stream_view_count=Count('stream_user_view_status')).select_related(
             'created_by__user_data__user').prefetch_related(
             Prefetch(
