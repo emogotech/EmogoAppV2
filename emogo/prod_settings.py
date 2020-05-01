7e62833cfd573b10daa9c0f7c79ead9a88c28d1d
ranjeet-northout <ranjeet.bijore@northout.com>
2020-05-01 12:23:00 +0530

    Update prod_settings.py

--- emogo/prod_settings.py
+++ emogo/prod_settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'https://stage.emogo.co/static/'
+STATIC_URL = '/static/'
 
 # global status for application
 STATUSES = (
