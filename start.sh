05a63ab9e9c72655d658eb7a3281a951acef82eb
Jonathan Woods <32648249+jonsaxton@users.noreply.github.com>
2020-05-21 18:23:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,7 +8,26 @@ echo "Docker container has been started"
 #celery -A sporttechie worker -l info -B > /dev/null 2>&1 &
 
 echo "Docker container has been started"
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.3.0.1492-linux.zip
+unzip sonar-scanner-cli-3.3.0.1492-linux.zip
+export PATH=$PATH:./sonar-scanner-3.3.0.1492-linux/bin/
+export SONAR_SCANNER_OPTS="-Xmx512m"
+mkdir -p /codebuild/output/src857048737/src/sonar-scanner-3.3.0.1492-linux/conf/
+cp sonar-scanner.properties /codebuild/output/src857048737/src/sonar-scanner-3.3.0.1492-linux/conf/sonar-scanner.properties
+cat /codebuild/output/src857048737/src/sonar-scanner-3.3.0.1492-linux/conf/sonar-scanner.properties
+cp sonar-scanner.properties sonar-scanner-3.3.0.1492-linux/conf/sonar-scanner.properties
 
+# python3.6 manage.py test -k
+
+#echo $?
+
+# if [ $? -eq 0 ]
+# then
+#   echo "posting data to sonarqube after maven build"
+sonar-scanner -Dsonar.projectKey=$KEY -Dsonar.sources=. -Dsonar.host.url=$URL -Dsonar.login=$TOKEN
+# else
+#   echo "Build failed"
+# fi
 python3.6 manage.py runserver 0.0.0.0:80 --settings=emogo.qa_settings
 
 exec "$@"
