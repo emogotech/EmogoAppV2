fe6e245bca305c388ea6d7ad4e9b756b2b24e0f6
Ranjeet <ranjeet.bijore@northout.com>
2020-07-03 13:06:11 +0530

    Fix Bug

--- emogo/lib/custom_filters/filterset.py
+++ emogo/lib/custom_filters/filterset.py
@@ -5,8 +5,8 @@ from django.db.models import Q
 from emogo.apps.collaborator.models import Collaborator
 from django.db.models import Prefetch , Count
 from emogo.apps.stream.models import StreamUserViewStatus, StarredStream, Folder
-# from django.core.exceptions import ObjectDoesNotExist
-# from django.http import Http404
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from itertools import chain
@@ -20,19 +20,19 @@ class StreamFilter(django_filters.FilterSet):
     name = django_filters.filters.CharFilter(method='filter_name')
     collaborator_qs = Collaborator.actives.all()
     stream_name = django_filters.filters.CharFilter(method='filter_stream_name')
-    # folder = django_filters.filters.CharFilter(method='filter_by_folder')
+    folder = django_filters.filters.CharFilter(method='filter_by_folder')
 
     class Meta:
         model = Stream
-        fields = ['stream_name', 'featured', 'emogo', 'my_stream', 'popular',
-                  'self_created', 'folder']
+        fields = ['folder', 'stream_name', 'featured', 'emogo', 'my_stream', 'popular',
+                  'self_created']
 
-    # def filter_by_folder(self, qs, name, value):
-    #     try:
-    #         folder = Folder.objects.get(id=value)
-    #     except ObjectDoesNotExist:
-    #         raise Http404("Folder does not exist.")
-    #     return qs.filter(folder=folder)
+    def filter_by_folder(self, qs, name, value):
+        try:
+            folder = Folder.objects.get(id=value)
+        except ObjectDoesNotExist:
+            raise Http404("Folder does not exist.")
+        return qs.filter(folder=folder)
     
     def filter_stream_name(self, qs, name, value):
         return qs.filter(name__icontains=value)
@@ -236,10 +236,10 @@ class ContentsFilter(django_filters.FilterSet):
         fields = ['type']
 
     def filter_by_stream(self, qs, name, value):
-        # try:
-        #     Stream.objects.get(id=value)
-        # except ObjectDoesNotExist:
-        #     raise Http404("Emogo does not exist.")
+        try:
+            Stream.objects.get(id=value)
+        except ObjectDoesNotExist:
+            raise Http404("Emogo does not exist.")
         return qs.filter(content_streams__stream=value)
 
 
