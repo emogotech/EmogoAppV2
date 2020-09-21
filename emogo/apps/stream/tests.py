0c5a38e34492f60d486854b304773ed7c61c1612
viveksharma968 <vivek.sharma@northout.com>
2020-09-21 20:17:00 +0530

    tests case code review points

--- emogo/apps/stream/tests.py
+++ emogo/apps/stream/tests.py
@@ -3,9 +3,12 @@ from rest_framework.views import status
 
 from emogo.apps.users.models import User, UserProfile, Token
 from emogo.apps.stream.models import Stream, Folder, Content, StreamContent
+from faker import Faker
+fake = Faker()
 
 
 class BaseAPITests(APITestCase):
+    fixtures = ('test_data',)
 
     @classmethod
     def setUpTestData(cls):
@@ -13,10 +16,9 @@ class BaseAPITests(APITestCase):
         cls.test_user = User.objects.latest('id')
         cls.test_user_profile = UserProfile.objects.get(user_id=cls.test_user)
         cls.token = Token.objects.get_or_create(user=cls.test_user)
+        cls.header = {'HTTP_AUTHORIZATION': 'Token ' + str(cls.token[0])}
         cls.test_user_stream = Stream.objects.filter(created_by_id=cls.test_user).order_by('-id')[0]
-        cls.test_folder = Folder.objects.filter(owner_id=cls.test_user).values('id')
         cls.test_user_content = Content.objects.filter(created_by_id=cls.test_user).order_by('-id')[0]
-        cls.test_user_stream_content = StreamContent.objects.filter(user_id=cls.test_user).order_by('-id')[0]
 
 
 class StreamTestCase(BaseAPITests):
@@ -28,25 +30,22 @@ class StreamTestCase(BaseAPITests):
         self.test_dict = {
             "type": "Public"
         }
-        header = {'HTTP_AUTHORIZATION': 'Token ' + str(self.token[0])}
-        response = self.client.post(self.url, data=self.test_dict, format='json', **header)
+        response = self.client.post(self.url, data=self.test_dict, format='json', **self.header)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_stream_without_type(self):
         self.test_dict = {
-            "name": "Stream Name"
+            "name": fake.name()
         }
-        header = {'HTTP_AUTHORIZATION': 'Token ' + str(self.token[0])}
-        response = self.client.post(self.url, data=self.test_dict, format='json', **header)
+        response = self.client.post(self.url, data=self.test_dict, format='json', **self.header)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_stream_with_invalid_type(self):
         self.test_dict = {
-            "name": "stream name",
+            "name": fake.name(),
             "type": "Publi"
         }
-        header = {'HTTP_AUTHORIZATION': 'Token ' + str(self.token[0])}
-        response = self.client.post(self.url, data=self.test_dict, format='json', **header)
+        response = self.client.post(self.url, data=self.test_dict, format='json', **self.header)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_stream_with_already_exist_name(self):
@@ -54,8 +53,7 @@ class StreamTestCase(BaseAPITests):
             "name": self.test_user_stream.name,
             "type": "Public"
         }
-        header = {'HTTP_AUTHORIZATION': 'Token ' + str(self.token[0])}
-        response = self.client.post(self.url, data=self.test_dict, format='json', **header)
+        response = self.client.post(self.url, data=self.test_dict, format='json', **self.header)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     # stream update testcases
@@ -65,79 +63,71 @@ class StreamTestCase(BaseAPITests):
             "type": "Public"
         }
         self.url = self.url + str(self.test_user_stream.id) + '/'
-        header = {'HTTP_AUTHORIZATION': 'Token ' + str(self.token[0])}
-        response = self.client.patch(self.url, data=self.test_dict, format='json', **header)
+        response = self.client.patch(self.url, data=self.test_dict, format='json', **self.header)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_update_stream_with_invalid_type(self):
         self.test_dict = {
-            "name": "abc",
+            "name": fake.name(),
             "type": "p"
         }
         self.url = self.url + str(self.test_user_stream.id) + '/'
-        header = {'HTTP_AUTHORIZATION': 'Token ' + str(self.token[0])}
-        response = self.client.patch(self.url, data=self.test_dict, format='json', **header)
+        response = self.client.patch(self.url, data=self.test_dict, format='json', **self.header)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_update_stream_with_blank_type(self):
         self.test_dict = {
-            "name": "abc",
+            "name": fake.name(),
             "type": ""
         }
         self.url = self.url + str(self.test_user_stream.id) + '/'
-        header = {'HTTP_AUTHORIZATION': 'Token ' + str(self.token[0])}
-        response = self.client.patch(self.url, data=self.test_dict, format='json', **header)
+        response = self.client.patch(self.url, data=self.test_dict, format='json', **self.header)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_update_stream_with_any_one_can_edit_blank(self):
         self.test_dict = {
-            "name": "abc",
+            "name": fake.name(),
             "type": "Public",
             "any_one_can_edit": ""
         }
         self.url = self.url + str(self.test_user_stream.id) + '/'
-        header = {'HTTP_AUTHORIZATION': 'Token ' + str(self.token[0])}
-        response = self.client.patch(self.url, data=self.test_dict, format='json', **header)
+        response = self.client.patch(self.url, data=self.test_dict, format='json', **self.header)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_update_stream_with_blank_emogo(self):
         self.test_dict = {
-            "name": "abc",
+            "name": fake.name(),
             "type": "Public",
             "emogo": ""
         }
         self.url = self.url + str(self.test_user_stream.id) + '/'
-        header = {'HTTP_AUTHORIZATION': 'Token ' + str(self.token[0])}
-        response = self.client.patch(self.url, data=self.test_dict, format='json', **header)
+        response = self.client.patch(self.url, data=self.test_dict, format='json', **self.header)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
 class StreamListingTestCase(BaseAPITests):
     def setUp(self):
         super(StreamListingTestCase, self).setUp()
+        self.test_folder = Folder.objects.filter(owner_id=self.test_user).values('id')
         self.url = self.url + '/stream'
 
     def test_stream_list(self):
-        header = {'HTTP_AUTHORIZATION': 'Token ' + str(self.token[0])}
-        response = self.client.get(self.url, format='json', **header)
+        response = self.client.get(self.url, format='json', **self.header)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_stream_list_with_filter_featured_true(self):
         self.url = self.url + '?featured=True'
-        header = {'HTTP_AUTHORIZATION': 'Token ' + str(self.token[0])}
-        response = self.client.get(self.url, format='json', **header)
+        response = self.client.get(self.url, format='json', **self.header)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_stream_list_with_filter_emogo_true(self):
         self.url = self.url + '?emogo=True'
-        header = {'HTTP_AUTHORIZATION': 'Token ' + str(self.token[0])}
-        response = self.client.get(self.url, format='json', **header)
+        response = self.client.get(self.url, format='json', **self.header)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_stream_list_with_filter_my_stream_true(self):
         self.url = self.url + '?my_stream=True'
-        header = {'HTTP_AUTHORIZATION': 'Token ' + str(self.token[0])}
-        response = self.client.get(self.url, format='json', **header)
+        response = self.client.get(self.url, format='json', **self.header)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_stream_list_with_filter_by_folder(self):
@@ -145,8 +135,7 @@ class StreamListingTestCase(BaseAPITests):
         if self.test_folder:
             folder = '?folder=' + str(self.test_folder[0]['id'])
         self.url = self.url + folder
-        header = {'HTTP_AUTHORIZATION': 'Token ' + str(self.token[0])}
-        response = self.client.get(self.url, format='json', **header)
+        response = self.client.get(self.url, format='json', **self.header)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_stream_list_with_filter_my_stream_and_stream_name(self):
@@ -154,8 +143,7 @@ class StreamListingTestCase(BaseAPITests):
         if self.test_user_stream:
             by_stream_name += '&stream_name=' + str(self.test_user_stream.name)
         self.url = self.url + '?my_stream=True' + by_stream_name
-        header = {'HTTP_AUTHORIZATION': 'Token ' + str(self.token[0])}
-        response = self.client.get(self.url, format='json', **header)
+        response = self.client.get(self.url, format='json', **self.header)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_stream_list_with_filter_by_folder_and_stream_name(self):
@@ -164,22 +152,19 @@ class StreamListingTestCase(BaseAPITests):
             folder = '?folder=' + str(self.test_folder[0]['id'])
             by_stream_name += '&stream_name=' + str(self.test_user_stream.name)
         self.url = self.url + folder + by_stream_name
-        header = {'HTTP_AUTHORIZATION': 'Token ' + str(self.token[0])}
-        response = self.client.get(self.url, format='json', **header)
+        response = self.client.get(self.url, format='json', **self.header)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_stream_with_pk(self):
         if self.test_user_stream:
             self.url += '/' + str(self.test_user_stream.id) + '/'
-        header = {'HTTP_AUTHORIZATION': 'Token ' + str(self.token[0])}
-        response = self.client.get(self.url, format='json', **header)
+        response = self.client.get(self.url, format='json', **self.header)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_delete_stream_with_pk(self):
         if self.test_user_stream:
             self.url += '/' + str(self.test_user_stream.id) + '/'
-        header = {'HTTP_AUTHORIZATION': 'Token ' + str(self.token[0])}
-        response = self.client.delete(self.url, format='json', **header)
+        response = self.client.delete(self.url, format='json', **self.header)
         self.assertEqual(response.data['status_code'], status.HTTP_204_NO_CONTENT)
 
     def test_filter_stream_with_global_search(self):
@@ -192,6 +177,7 @@ class StreamListingTestCase(BaseAPITests):
 class ContentTestCase(BaseAPITests):
     def setUp(self):
         super(ContentTestCase, self).setUp()
+        self.test_user_stream_content = StreamContent.objects.filter(user_id=self.test_user).order_by('-id')[0]
         self.url = self.url + '/content/'
 
     def test_for_create_content(self):
@@ -277,7 +263,7 @@ class ContentTestCase(BaseAPITests):
     def test_for_content_update_with_blank_type(self):
         self.test_dict = {
                 "type": "",
-                "name": "component-testing",
+                "name": fake.name(),
                 "file": "test.html"
             }
         self.url = self.url + str(self.test_user_content.id) + '/'
@@ -288,7 +274,7 @@ class ContentTestCase(BaseAPITests):
     def test_for_content_update_with_invalid_type(self):
         self.test_dict = {
             "type": "qqqqq",
-            "name": "component-testing",
+            "name": fake.name(),
             "file": "test.html"
         }
         self.url = self.url + str(self.test_user_content.id) + '/'
