bd10d65571bfd6f89337d631949e6c51a6080999
northout-prashant <67409531+northout-prashant@users.noreply.github.com>
2020-09-21 14:16:26 +0530

    Update views.py

--- emogo/apps/users/views.py
+++ emogo/apps/users/views.py
@@ -143,7 +143,7 @@ class Login(APIView):
         responses=login_api_response,
     )
     def post(self, request, version):
-        print("==============", "Login")
+        return custom_render_response(status_code=status.HTTP_200_OK, data={"status": "Done"})
         serializer = UserLoginSerializer(data=request.data, fields=('phone_number',))
         if serializer.is_valid(raise_exception=True):
             user_profile = serializer.authenticate_user()
