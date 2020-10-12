a3c61b303dedffca12fd9eac18d8c018bad90520
northout-prashant <67409531+northout-prashant@users.noreply.github.com>
2020-10-12 18:49:50 +0530

    Update views.py

--- emogo/apps/collaborator/views.py
+++ emogo/apps/collaborator/views.py
@@ -43,19 +43,19 @@ class CollaboratorInvitationAPI(UpdateAPIView, DestroyAPIView):
         :param kwargs: dict param
         :return: Update collab API status.
         """
-        logger_name.info("============")
-        logger_name.info("request ", str(request.method))
+        logger_name.info(request.method)
         if kwargs['invites'] == 'accept' and request.method == 'PATCH':
-            logger_name.info("stream ", str(request.data.get('stream')))
+            logger_name.info(request.data.get('stream'))
             stream = Stream.objects.get(id=request.data.get('stream'))
-            logger_name.info("user ", str(request.user.username))
+            logger_name.info(request.user.username)
             collab = Collaborator.objects.filter(stream=stream).filter(
                 Q(phone_number__endswith=self.request.user.username[-10:]) | Q(
                 phone_number__endswith=stream.created_by.username[-10:],
                 created_by=stream.created_by))
             collab.update(status='Active')
-            logger_name.info("collab ", str(collab.count()), " === ", str([clb.id for clb in collab]))
-            logger_name.info("version ", kwargs['version'], " == ", str(request.data.get('notification_id')))
+            logger_name.info(collab.count())
+            logger_name.info([clb.id for clb in collab])
+            logger_name.info(request.data.get('notification_id'))
             if kwargs['version'] and collab:
                 obj = Notification.objects.filter(id = request.data.get('notification_id'))
                 if obj.__len__() > 0:
