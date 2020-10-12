a283a1cf6534dfd54a5e7ca4364208e534d62737
northout-prashant <prashant.chouhan@northout.com>
2020-10-12 17:58:15 +0530

    Add logger for collab accept api

--- emogo/apps/collaborator/views.py
+++ emogo/apps/collaborator/views.py
@@ -21,6 +21,8 @@ from emogo.apps.notification.serializers import *
 from emogo.apps.stream.serializers import ViewStreamSerializer
 
 from emogo.apps.collaborator.serializers import ViewCollaboratorSerializer
+import logging
+logger_name = logging.getLogger('email_log')
 
 # # Create your views here.
 class CollaboratorInvitationAPI(UpdateAPIView, DestroyAPIView):
@@ -41,14 +43,21 @@ class CollaboratorInvitationAPI(UpdateAPIView, DestroyAPIView):
         :param kwargs: dict param
         :return: Update collab API status.
         """
+        logger_name.info("============")
+        logger_name.info("invites ", kwargs['invites'])
+        logger_name.info("request ", request.method)
         if kwargs['invites'] == 'accept' and request.method == 'PATCH':
+            logger_name.info("stream ", request.data.get('stream'))
             stream = Stream.objects.get(id=request.data.get('stream'))
+            logger_name.info("user ", request.user.username)
             collab = Collaborator.objects.filter(stream=stream).filter(
                 Q(phone_number__endswith=self.request.user.username[-10:]) | Q(
                 phone_number__endswith=stream.created_by.username[-10:],
                 created_by=stream.created_by))
             collab.update(status='Active')
-            if kwargs['version']:
+            logger_name.info("collab ", collab.count(), " === ", [clb.id for clb in collab])
+            logger_name.info("version ", kwargs['version'], " == ", request.data.get('notification_id'))
+            if kwargs['version'] and collab:
                 obj = Notification.objects.filter(id = request.data.get('notification_id'))
                 if obj.__len__() > 0:
                     if not (obj[0].notification_type == 'deleted_collaborator' or obj[0].notification_type == 'deleted_stream' ):
