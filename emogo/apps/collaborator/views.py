53eea5129a0ae33fea4f241fd770fbd3ead1b015
northout-prashant <67409531+northout-prashant@users.noreply.github.com>
2020-11-02 13:23:12 +0530

    Update views.py

--- emogo/apps/collaborator/views.py
+++ emogo/apps/collaborator/views.py
@@ -22,6 +22,8 @@ from emogo.apps.stream.serializers import ViewStreamSerializer
 
 from emogo.apps.collaborator.serializers import ViewCollaboratorSerializer
 from django.http import Http404
+import logging
+logger_name = logging.getLogger('email_log')
 
 # # Create your views here.
 class CollaboratorInvitationAPI(UpdateAPIView, DestroyAPIView):
@@ -49,6 +51,8 @@ class CollaboratorInvitationAPI(UpdateAPIView, DestroyAPIView):
                 phone_number__endswith=stream.created_by.username[-10:],
                 created_by=stream.created_by))
             collab.update(status='Active')
+            logger_name.info("======================")
+            logger_name.info(collab)
             if kwargs['version'] and collab:
                 obj = Notification.objects.filter(id = request.data.get('notification_id'))
                 if obj.__len__() > 0:
@@ -58,6 +62,7 @@ class CollaboratorInvitationAPI(UpdateAPIView, DestroyAPIView):
                         NotificationAPI().send_notification(self.request.user, obj[0].from_user, 'accepted', stream)
                     serializer = self.get_serializer(obj[0], context=self.request)
                     return custom_render_response(status_code=status.HTTP_200_OK, data=serializer.data)
+            return custom_render_response(status_code=status.HTTP_404_NOT_FOUND)    
             # To return accpted
         else:
             return custom_render_response(status_code=status.HTTP_404_NOT_FOUND)
