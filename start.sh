c97205760deee7037630fa9d37a5b197f40e36f1
Jonathan Woods <32648249+jonsaxton@users.noreply.github.com>
2020-11-04 18:11:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -30,7 +30,7 @@ sonar-scanner -Dsonar.projectKey=$KEY -Dsonar.sources=. -Dsonar.host.url=$URL -D
 # else
 #   echo "Build failed"
 # fi
-python3.6 manage.py runserver 0.0.0.0:80 --settings=emogo.settings
+#python3.6 manage.py runserver 0.0.0.0:80 --settings=emogo.settings
 uwsgi --http :80 --module emogo.wsgi --process 4
 
 
