594960e02bb132bef73042bb9193e80b1798b732
ranjeet-northout <ranjeet.bijore@northout.com>
2020-11-25 18:10:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -30,7 +30,8 @@ sonar-scanner -Dsonar.projectKey=$KEY -Dsonar.sources=. -Dsonar.host.url=$URL -D
 # else
 #   echo "Build failed"
 # fi
-python3.6 manage.py runserver 0.0.0.0:80 --settings=emogo.settings
+#python3.6 manage.py runserver 0.0.0.0:80 --settings=emogo.settings
+daphne -p 80 emogo.asgi:application
 #uwsgi --http :80 --module emogo.wsgi --process 10 --master
 
 exec "$@"
