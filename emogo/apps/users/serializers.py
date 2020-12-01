d909987e0cda4bfb8725b06176c9ada6044d9ca5
ranjeet-northout <ranjeet.bijore@northout.com>
2020-12-01 13:48:03 +0530

    Update serializers.py

--- emogo/apps/users/serializers.py
+++ emogo/apps/users/serializers.py
@@ -56,8 +56,8 @@ class UserSerializer(DynamicFieldsModelSerializer):
         # The code is run while user was not verified but try to sign-up with different user_name or phone number
         # 1. While user request with same user_name and different phone number
         body =  "Emogo sign up OTP"
-        #sent_otp = send_otp(validated_data.get('username'), body)  # Todo Uncomment this code before move to stage server
-        sent_otp = 12345
+        sent_otp = send_otp(validated_data.get('username'), body)  # Todo Uncomment this code before move to stage server
+        #sent_otp = 12345
         if sent_otp is not None:
             setattr(self, 'user_pin', sent_otp)
         else:
