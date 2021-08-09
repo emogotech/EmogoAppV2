c4747ee535de96e70a64573fbeb05017133bfdab
northoutSwarnim <swarnim.shrotriya@northout.com>
2021-08-09 05:24:10 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,15 +9,15 @@ service redis-server start
 
 #celery -A sporttechie worker -l info -B > /dev/null 2>&1 &
 
-echo "Docker container has been started"
-wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.3.0.1492-linux.zip
-unzip sonar-scanner-cli-3.3.0.1492-linux.zip
-export PATH=$PATH:./sonar-scanner-3.3.0.1492-linux/bin/
-export SONAR_SCANNER_OPTS="-Xmx512m"
-mkdir -p /codebuild/output/src857048737/src/sonar-scanner-3.3.0.1492-linux/conf/
-cp sonar-scanner.properties /codebuild/output/src857048737/src/sonar-scanner-3.3.0.1492-linux/conf/sonar-scanner.properties
-cat /codebuild/output/src857048737/src/sonar-scanner-3.3.0.1492-linux/conf/sonar-scanner.properties
-cp sonar-scanner.properties sonar-scanner-3.3.0.1492-linux/conf/sonar-scanner.properties
+# echo "Docker container has been started"
+# wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.3.0.1492-linux.zip
+# unzip sonar-scanner-cli-3.3.0.1492-linux.zip
+# export PATH=$PATH:./sonar-scanner-3.3.0.1492-linux/bin/
+# export SONAR_SCANNER_OPTS="-Xmx512m"
+# mkdir -p /codebuild/output/src857048737/src/sonar-scanner-3.3.0.1492-linux/conf/
+# cp sonar-scanner.properties /codebuild/output/src857048737/src/sonar-scanner-3.3.0.1492-linux/conf/sonar-scanner.properties
+# cat /codebuild/output/src857048737/src/sonar-scanner-3.3.0.1492-linux/conf/sonar-scanner.properties
+# cp sonar-scanner.properties sonar-scanner-3.3.0.1492-linux/conf/sonar-scanner.properties
 
 # python3.6 manage.py test -k
 
@@ -26,7 +26,7 @@ cp sonar-scanner.properties sonar-scanner-3.3.0.1492-linux/conf/sonar-scanner.pr
 # if [ $? -eq 0 ]
 # then
 #   echo "posting data to sonarqube after maven build"
-sonar-scanner -Dsonar.projectKey=$KEY -Dsonar.sources=. -Dsonar.host.url=$URL -Dsonar.login=$TOKEN
+# sonar-scanner -Dsonar.projectKey=$KEY -Dsonar.sources=. -Dsonar.host.url=$URL -Dsonar.login=$TOKEN
 # else
 #   echo "Build failed"
 # fi
