edc1a225e694fe95b7cc751be3d16ed3e8ede1c9
kripa <54672823+northoutkripa@users.noreply.github.com>
2021-10-19 17:21:22 +0530

    Create install_dependencies.sh
    new script

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+
+cd /home/ubuntu
+rm -rf EmogoAppV2/*
+rm -rf EmogoAppV2/.git
+rm -rf EmogoAppV2/.gitignore
+rm -rf EmogoAppV2/.ebextensions
+
+
+
+echo "successfully installed"
+#sudo apt-get update
+#sudo apt-get install -y rabbitmq-server gcc
+# INSTALL syslog for debugging my cron jobs
+#sudo apt-get install -y rsyslog
+
+
+
+# Optional: depending on your environment
+#sudo apt-get install -y default-jdk
