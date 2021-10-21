5929ee7fe609f529439c92c5120b8c35c4d25699
ranjeet-northout <ranjeet.bijore@northout.com>
2021-10-21 18:43:22 +0530

    Update install_requirements.sh

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -2,18 +2,12 @@
 service rabbitmq-server start
 export C_FORCE_ROOT='true'
 
-
-
 source /home/ubuntu/EmogoAppV2/venv/bin/activate
 mkdir /home/ubuntu/EmogoAppV2/logs && chmod 777 /home/ubuntu/EmogoAppV2/logs
 touch /home/ubuntu/EmogoAppV2/logs/debug.log
 cd /home/ubuntu/EmogoAppV2
 
-
-
 /home/ubuntu/EmogoAppV2/venv/bin/pip install --no-cache-dir -r requirements.txt
-
-
 
 #/home/ubuntu/EmogoAppV2/venv/bin/python manage.py makemigrations --merge
 /home/ubuntu/EmogoAppV2/venv/bin/python manage.py migrate
